54a68198c75b55f2ceb7c4463f2e8e3e734775d3
gurudarshan266 <gpollep@ncsu.edu>
2017-03-11 14:38:26 -0500

    Add, delete in RfcManager

--- src/RFC.java
+++ src/RFC.java
@@ -3,21 +3,24 @@ public class RFC implements Comparable<RFC>{
 	
 	int rfc_num;
 	String title;
+	String host_name;
+	Peer peer;
 	
-	public RFC(int num, String title)
+	public RFC(int num, String title, String host_name)
 	{
 		this.rfc_num = num;
 		this.title = title;
+		this.host_name = host_name;
+		this.peer = PeerManager.getPeerManager().locatePeer(host_name);
 	}
 
 	@Override
 	public int compareTo(RFC other) {
-		if(this.rfc_num == other.rfc_num && title.toLowerCase().compareTo(other.title)==0)//Check title and RFC number
-			return 1;
+		if(this.rfc_num == other.rfc_num)// && title.toLowerCase().compareTo(other.title)==0)//Check title and RFC number
+			return 0;
 		
-		return 0;
+		return -1;
 	}
-	
-	
+
 
 }
--- src/RfcManager.java
+++ src/RfcManager.java
@@ -4,8 +4,79 @@ public class RfcManager {
 
 	private ArrayList<RFC> rfc_list;
 	
-	public RfcManager()
+	private static RfcManager instance;
+	
+	private RfcManager()
 	{
 		rfc_list = new ArrayList<RFC>();
+	}
+	
+	public static RfcManager getRfcManager()
+	{
+		if(instance == null) instance = new RfcManager();
+		return instance;
+	}
+	
+	/*
+	 * Find RFC by number
+	 */
+	public ArrayList<Peer> findRFC(int rfc_num)
+	{
+		ArrayList<Peer> peerList = new ArrayList<>(); 
+		
+		for(RFC r : this.rfc_list)
+		{
+			if(r.rfc_num == rfc_num)
+				peerList.add(r.peer);
+		}
+		
+		return peerList;
+	}
+	
+	/*
+	 * Find RFC by title 
+	 */
+	public ArrayList<Peer> findRFC(String title) {
+		
+		ArrayList<Peer> peerList = new ArrayList<>();
+
+		for (RFC r : this.rfc_list) 
+		{
+			if (title.compareToIgnoreCase(r.title) == 0)
+				peerList.add(r.peer);
+		}
+
+		return peerList;
+	}
+	
+	public void addRfc(int num, String title, String hostname)
+	{
+		RFC r = new RFC(num,title,hostname);
+		rfc_list.add(r);
+	}
+	
+	public void addRfc(RFC r)
+	{
+		rfc_list.add(r);
+	}
+	
+	private int getIndex(int rfc_num)
+	{
+		for(int i=0;i<rfc_list.size();i++)
+		{
+			if(rfc_list.get(i).rfc_num == rfc_num)
+				return i;
+		}
+		
+		return -1;
+	}
+
+	public void deleteRfc(int rfc_num) {
+
+		int index = getIndex(rfc_num);
+		
+		while(index > 0) {
+			rfc_list.remove(index);
+		}
 	}
 }
