6924cacd397dfbdaed22c5a1ae5c11bd69ccf800
gurudarshan266 <gpollep@ncsu.edu>
2017-03-11 15:34:05 -0500

    Added RequestP2P class

---
+++ src/RequestP2P.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class RequestP2P {
+	
+	HashMap<String,String> headers;
+	
+	String method;
+	int rfcNum;
+	String version;
+
+	public RequestP2P(Scanner sc)
+	{
+		parseFirstLine(sc);
+		
+		while(parseLine(sc));
+	}
+	
+	private void parseFirstLine(Scanner sc)
+	{
+		this.method = sc.next();
+		sc.next();//Ignore "RFC"
+		this.rfcNum = sc.nextInt();
+		this.version = sc.nextLine();
+	}
+	
+	private boolean parseLine(Scanner sc)
+	{
+		String line = sc.nextLine();
+		
+		if(line.compareTo("")==0 || line==null)
+			return false;
+		
+		String tokens[] = line.split(": ");
+		headers.put(tokens[0], tokens[1]);
+		return true;
+		
+	}
+}
